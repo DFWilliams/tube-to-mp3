310299a1a8d62bd021470b334782ede2d4fcbfa3
Deanna "Will" Williams <deannawilliams@Deannas-MacBook-Pro.local>
2023-11-17 13:45:44 -0800

    added simple css to start off with

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-
+import "./App.css"
 
 const App = () => {
   return (
-    <> 
-    <h1>YO!</h1>
+    <>
+      <span className="logo">VibeLinkz</span>
+      <div className="page_content">
+        <h1>Youtube to Mp3</h1>
+        <h3>Take you favorite videos and turn them into your favorite tunes</h3>
+        <input placeholder="Paste your YouTube link here" type="text" className="link_input"></input>
+      </div>
+      <br/>
+      <div className="conversion_btn">
+        <button className="btn">Convert</button>
+      </div>
+      <br/>
+      <div className="link">
+        <a>Download Link</a>
+      </div>
     </>
   )
 }
